a4842a13f21bdeb1fb3f3be7acb8f9130e50cb09
server <server@server>
2022-03-19 01:46:48 -0300

    logs php

---
+++ LogsE.php
@@ -0,0 +1,30 @@
+<?php
+
+//namespace de acordo com projeto
+use Exception;
+
+class LogsE
+{
+
+    public  static function escrever($mensagem)
+    {
+        $logs = config('app.logs');
+        if ($logs == 1) {
+            try {
+                $arquivo = fopen('../logs/logs.log', 'a+');
+                $texto = " ";
+                $texto .=  "### data log ---> " . date('d/m/y H:i:s');
+                $texto .= "  >>>>> : " . $mensagem . "  >>>>> #";
+                $texto .= PHP_EOL;
+                fwrite($arquivo, $texto);
+                fclose($arquivo);
+            } catch (Exception $e) {
+                $erro = '<script>';
+                $erro .= 'console.log('. json_encode( $e->getMessage()) .')' ;
+                $erro .= '</script>';
+                echo $erro;
+                
+            }
+        }
+    }
+}
